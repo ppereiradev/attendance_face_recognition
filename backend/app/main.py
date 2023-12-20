03f29b9ad03b80600409708ade88e102943e576d
Paulo Pereira <paulo.pereira.phd@gmail.com>
2023-12-20 16:00:32 -0300

    Initial commit

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+import uvicorn
+
+from typing import Union
+from fastapi import FastAPI
+from deepface import DeepFace
+import json
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    verification = DeepFace.verify(img1_path = "img1.jpeg", img2_path = "img2.jpeg")
+    verification['verified'] = "true" if verification['verified'] else "false"
+    return verification
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
